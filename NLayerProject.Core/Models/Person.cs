34e7b1c16094318421c760c21cd04da99c8b18ee
oguzhansenturk <oguzhansenturk@gmail.com>
2021-07-28 23:18:41 +0300

    person add

---
+++ NLayerProject.Core/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace NLayerProject.Core.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+    }
+
+}
--- NLayerProject.Data/AppDbContext.cs
+++ NLayerProject.Data/AppDbContext.cs
@@ -15,6 +15,7 @@ namespace NLayerProject.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<Person> Persons { get; set; }
 
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -24,6 +25,13 @@ namespace NLayerProject.Data
 
             modelBuilder.ApplyConfiguration(new ProductSeed(new int[] { 1, 2 }));
             modelBuilder.ApplyConfiguration(new CategorySeed(new int[] {1,2 }));
+
+
+            modelBuilder.Entity<Person>().HasKey(x => x.Id);
+            modelBuilder.Entity<Person>().Property(x => x.Id).UseIdentityColumn();
+            modelBuilder.Entity<Person>().Property(x => x.Name).HasMaxLength(100);
+            modelBuilder.Entity<Person>().Property(x => x.SurName).HasMaxLength(100);
+
         }
 
     }
