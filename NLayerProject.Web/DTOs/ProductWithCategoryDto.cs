c8160e75072fcd59c548343006d852e70090da95
Oguzhan <oguzhansenturk@gmail.com>
2021-07-30 11:52:41 +0300

    mvc project add

---
+++ NLayerProject.Web/DTOs/ProductWithCategoryDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace NLayerProject.Web.DTOs
+{
+    public class ProductWithCategoryDto : ProductDto
+    {
+        public ProductWithCategoryDto()
+        {
+        }
+
+        public CategoryDto Category { get; set; }
+    }
+}
---
+++ NLayerProject.Web/Filters/NotFoundFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using NLayerProject.Web.DTOs;
+using NLayerProject.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Web.Filters
+{
+    public class NotFoundFilter : ActionFilterAttribute
+    {
+        private readonly ICategoryService _categoryService;
+
+        public NotFoundFilter(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+        public async override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            int id = (int)context.ActionArguments.Values.FirstOrDefault();
+            var product = await _categoryService.GetByIdAsync(id);
+
+            if (product != null)
+            {
+                await next();
+            }
+            else
+            {
+                ErrorDTO errorDTO = new();
+                errorDTO.Errors.Add($"İd'si {id} olan kategori veritabanında bulunamadı");
+
+                context.Result = new RedirectToActionResult("Error", "Home", errorDTO);
+            }
+
+        }
+
+    }
+}
